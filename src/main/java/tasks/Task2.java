901b7fb116c96f726f1ada843af4e19b3389096d
Dmitrii <dmkurachev@gmail.com>
2026-02-18 13:44:19 +0600

    tasks: add solution for task 2

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -3,7 +3,9 @@ package tasks;
 import common.Person;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 /*
 Задача 2
@@ -14,8 +16,11 @@ import java.util.List;
 public class Task2 {
 
   public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
-                                                     Collection<Person> persons2,
-                                                     int limit) {
-    return new ArrayList<>();
+          Collection<Person> persons2,
+          int limit) {
+    return new ArrayList<>(Stream.concat(persons1.stream(), persons2.stream())
+            .sorted(Comparator.comparing(Person::createdAt))
+            .limit(limit)
+            .toList());
   }
 }
