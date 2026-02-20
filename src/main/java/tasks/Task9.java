eaf033396f3c0d3fa3fca64f017492f6045281f6
Dmitry <>
2026-02-20 22:10:25 +0600

    tasks: add solution for task 9

--- src/main/java/tasks/Task9.java
+++ src/main/java/tasks/Task9.java
@@ -1,14 +1,8 @@
 package tasks;
 
 import common.Person;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -26,69 +20,82 @@ public class Task9 {
   // Костыль, эластик всегда выдает в топе "фальшивую персону".
   // Конвертируем начиная со второй
   public List<String> getNames(List<Person> persons) {
-    if (persons.size() == 0) {
-      return Collections.emptyList();
-    }
-    persons.remove(0);
-    return persons.stream().map(Person::firstName).collect(Collectors.toList());
+    // Добавляем skip(1) для пропуска первого элемента стрима
+    return persons.stream().skip(1).map(Person::firstName).toList();
   }
 
   // Зачем-то нужны различные имена этих же персон (без учета фальшивой разумеется)
   public Set<String> getDifferentNames(List<Person> persons) {
-    return getNames(persons).stream().distinct().collect(Collectors.toSet());
+    // dictinct() тут излишен, потому что toSet() в любом случае отдаст нам уникальные элементы
+    // Следовательно, всё выражение можно просто заменить на конструктор HashSet
+    return new HashSet<>(getNames(persons));
   }
 
   // Тут фронтовая логика, делаем за них работу - склеиваем ФИО
   public String convertPersonToString(Person person) {
-    String result = "";
-    if (person.secondName() != null) {
-      result += person.secondName();
-    }
-
-    if (person.firstName() != null) {
-      result += " " + person.firstName();
-    }
-
-    if (person.secondName() != null) {
-      result += " " + person.secondName();
-    }
-    return result;
+    /*
+      Во-первых, мы тут зачем-то собираем ФИФ, а не ФИО
+      Во-вторых, текущая логика функции допускает возможность строки с лидирующим пробелом
+      В-третьих, код можно переписать используя стримы, что сделает его более читаемым и уберет дублирование кода
+      Также, в Person нет переопределенных конструкторов с валидацией полей, поэтому
+      предположу, что нужно отфильтровать персоны по null и пустым значениям
+    */
+    return Stream.of(person.secondName(), person.firstName(), person.middleName())
+                 .filter(Objects::nonNull)
+                 .filter(String::isBlank)
+                 .map(String::trim) // Убираем лишние пробелы с начала и конца строк
+                 .collect(Collectors.joining(" ")); // Собираем строку через пробелы
   }
 
   // словарь id персоны -> ее имя
   public Map<Integer, String> getPersonNames(Collection<Person> persons) {
-    Map<Integer, String> map = new HashMap<>(1);
-    for (Person person : persons) {
-      if (!map.containsKey(person.id())) {
-        map.put(person.id(), convertPersonToString(person));
-      }
-    }
-    return map;
+    // Указано, что нужно сопоставить id и имя персоны, но использован метод convertPersonToString,
+    // который возвращает ФИО полностью
+    // Если нужно именно имя, то вместо this::convertPersonToString нужно использовать Person::firstName
+    return persons.stream()
+                  .collect(Collectors.toMap(
+                      Person::id,
+                      this::convertPersonToString,
+                      // Решаем конфликты ключей, оставляя существующее значение
+                      (a, b) -> a
+                  ));
   }
 
   // есть ли совпадающие в двух коллекциях персоны?
   public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
-    boolean has = false;
-    for (Person person1 : persons1) {
-      for (Person person2 : persons2) {
-        if (person1.equals(person2)) {
-          has = true;
-        }
-      }
-    }
-    return has;
+    // Учитывая, что Person - это record без переопределения методов equals и hashCode,
+    // можем сформировать hashSet по первой коллекции персон и проверить, есть ли там элементы из второй коллекции
+    HashSet<Person> uniquePersons1 = new HashSet<>(persons1);
+    return persons2.stream().anyMatch(uniquePersons1::contains);
   }
 
   // Посчитать число четных чисел
   public long countEven(Stream<Integer> numbers) {
-    count = 0;
-    numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
-    return count;
+    // Можем использовать метод count вместо хранения отдельной переменной
+    return numbers.filter(num -> num % 2 == 0).count();
   }
 
   // Загадка - объясните почему assert тут всегда верен
   // Пояснение в чем соль - мы перетасовали числа, обернули в HashSet, а toString() у него вернул их в сортированном порядке
   void listVsSet() {
+    /*
+      Долго копался по исходникам коллекций, пытался найти особенности работы итераторов, но, как я понял в итоге,
+      всё дело в реализации hashCode для Integer, судя по которой, hash будет совпадать со значением числа, что
+      как раз и даст правильный порядок перебора, в каком бы порядке мы не клали числа.
+
+      Upd. Решил уточнить пару моментов, почему именно так происходит, так как разобрался поглубже
+
+      Порядок перебора сета из интов не всегда будет таким, так как он зависит от размера самого контейнера.
+      Так что можно гарантировать возрастающий порядок перебора элементов только в том случае, когда у нас
+      размер сета больше максимального элемента сета, потому что в таком случае каждый элемент будет попадать в бакет,
+      номер которого равен значению самого элемента.
+
+      Ну, и в нашем случае как-раз так и получается. Судя по реализации HashSet, при его инициализации на основе
+      другого контейнера, создается мапа с размером "Math.max((int) (c.size()/.75f) + 1, 16)", что по сути всегда
+      на ~33% больше, чем количество элементов в переданном контейнере. А так как у нас список с подряд идущими
+      числами, его размер равен значению максимального элемента.
+      Следовательно, условие "размер сета больше максимального элемента сета" соблюдается.
+     */
     List<Integer> integers = IntStream.rangeClosed(1, 10000).boxed().collect(Collectors.toList());
     List<Integer> snapshot = new ArrayList<>(integers);
     Collections.shuffle(integers);
