d0d0e7a222b7875ca36cab65cafd6c7ed38fd204
Dmitry <>
2026-02-24 17:45:50 +0600

    tasks: add solution for task 6

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -2,10 +2,11 @@ package tasks;
 
 import common.Area;
 import common.Person;
+
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Имеются
@@ -16,9 +17,24 @@ import java.util.Set;
  */
 public class Task6 {
 
+  // Посчитал, что вынести создание описания на основе персоны и региона добавит читаемости
+  private static String createDescription(Person person, Area area) {
+    return person.firstName() + " - " + area.getName();
+  }
+
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    // Чтобы ускорить поиск регионов, создаем мапу регионов по их id
+    Map<Integer, Area> areaById = areas.stream().collect(Collectors.toMap(Area::getId, a -> a));
+
+    return persons.stream()
+                  // Берем Set.of() как дефолтное значение, если персоны нет в переданной мапе
+                  .flatMap(person -> personAreaIds.getOrDefault(person.id(), Set.of())
+                                                  .stream()
+                                                  // Решил использовать два .map подряд для повышения читаемости
+                                                  .map(areaById::get)
+                                                  .map(area -> createDescription(person, area)))
+                  .collect(Collectors.toSet());
   }
 }
